e7fbab0d46f54012675764ecb123e2c2e32a0d20
Victor Campos <victor.htp@hotmail.com>
2021-12-02 15:45:18 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,3 +1,4 @@
+import { UnexpectedError } from "./../../../domain/errors/unexpected-error";
 import { InvalidCredentialsError } from "./../../../domain/errors/invalid-credentials-error";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
@@ -15,10 +16,12 @@ export class RemoteAuthentication {
       body: params,
     });
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break;
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError();
       default:
-        return Promise.resolve();
+        throw new UnexpectedError();
     }
   }
 }
