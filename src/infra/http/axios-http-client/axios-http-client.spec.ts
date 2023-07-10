798bbe4f280df90434bdb05d0e0de758ee3776a7
Victor Campos <victor.htp@hotmail.com>
2023-07-10 13:55:41 -0300

    feat: add tests

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,32 @@
+import { AxiosHttpClient } from "./axios-http-client"
+import axios from "axios"
+import { faker } from "@faker-js/faker"
+import { HttpPostParams } from "@/data/protocols/http"
+
+jest.mock("axios")
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
+
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: {}
+})
+
+describe("AxiosHttpClient", () => {
+  test("Should call Axios with correct url and verb", async () => {
+    const request = mockPostRequest()
+    const sut = makeSut()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test("Should return correct statusCode and body", async () => {
+    const request = mockPostRequest()
+    const sut = makeSut()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+})
