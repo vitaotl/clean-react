6d1e13e5994cc304ce79ac31af4a697ef87b77a9
Victor Campos <victor.htp@hotmail.com>
2023-07-13 20:09:45 -0300

    refactor: refactor login,header and footer

---
+++ src/presentation/pages/login/components/footer/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from './footer-styles.scss'
+
+const Footer: React.FC = () => {
+  return <footer className={styles.footer}>footer</footer>
+}
+
+export default Footer
---
+++ src/presentation/pages/login/components/login-header/index.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from "react"
+import Logo from "../logo/logo"
+
+import styles from "./login-reader-styles.scss"
+
+const LoginHeader: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <h1>4Dev - Enquetes para Programadores</h1>
+    </header>
+  )
+}
+
+export default memo(LoginHeader)
