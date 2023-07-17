5dfa86dbea2967b684e1d236521c11ca4fc516a1
Victor Campos <victor.htp@hotmail.com>
2023-07-17 08:58:04 -0300

    test: ensure email validation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFielError } from "@/validation/errors"
 import { EmailValidation } from "./email-validation"
+import { faker } from "@faker-js/faker"
 
 describe("EmailValidation", () => {
-  test("Shoul return error if email is invalid", () => {
+  test("Should return error if email is invalid", () => {
     const sut = new EmailValidation("email")
-    const error = sut.validate("")
+    const error = sut.validate(faker.word.words(5))
     expect(error).toEqual(new InvalidFielError("email"))
+  })
+
+  test("Should falsy if email is valid", () => {
+    const sut = new EmailValidation("email")
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
