c6b51c2e645722e93867287f0916041628522149
Victor Campos <victor.htp@hotmail.com>
2023-07-17 08:38:03 -0300

    refactor: create sut validation

--- src/validation/required-field/required-fiel-validation.spec.ts
+++ src/validation/required-field/required-fiel-validation.spec.ts
@@ -1,10 +1,20 @@
 import { RequiredFieldError } from "../errors"
 import { RequiredFieldValidation } from "./required-fiel-validation"
+import { faker } from "@faker-js/faker"
+
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(faker.database.column())
 
 describe("RequiredFieldValidation", () => {
   test("Shoul return error if field is empty", () => {
-    const sut = new RequiredFieldValidation("email")
-    const error = sut.validate("victor")
+    const sut = makeSut()
+    const error = sut.validate("")
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test("Shoul return falsy if field is not empty", () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.word.words(5))
+    expect(error).toBeFalsy()
   })
 })
