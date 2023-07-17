1223febef2917dd67ed1e9170a1fbda977809cf2
Victor Campos <victor.htp@hotmail.com>
2023-07-17 09:02:37 -0300

    refactor: create sut

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,15 +2,24 @@ import { InvalidFielError } from "@/validation/errors"
 import { EmailValidation } from "./email-validation"
 import { faker } from "@faker-js/faker"
 
+type SutTypes = {
+  sut: EmailValidation
+}
+
+const makeSut = (field: string): SutTypes => {
+  const sut = new EmailValidation(field)
+  return { sut }
+}
+
 describe("EmailValidation", () => {
   test("Should return error if email is invalid", () => {
-    const sut = new EmailValidation("email")
+    const { sut } = makeSut("email")
     const error = sut.validate(faker.word.words(5))
     expect(error).toEqual(new InvalidFielError("email"))
   })
 
   test("Should falsy if email is valid", () => {
-    const sut = new EmailValidation("email")
+    const { sut } = makeSut("email")
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
